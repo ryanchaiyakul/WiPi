ed8323ec367a318d751cf5bf23eb9102c75e0d92
ryanchaiyakul <ryanchaiyakul@gmail.com>
2019-11-02 18:10:52 -0700

    changed file structure

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import linux, win64
-
-def getWifi(interface:str)->Wifi:
-        if sys.platform == "linux":
-            return linux.WifiLinux(interface)
-        return win64.WifiWin(interface)
