670332f2ca9277c34ba31bbcae0a7a1ae9752337
ryanchaiyakul <ryanchaiyakul@gmail.com>
2019-11-28 15:34:58 -0800

    added getAccessPoint

--- WiPi/__init__.py
+++ WiPi/__init__.py
@@ -1,7 +1,12 @@
 import sys
-from . import linux, win32
+from . import linux, win32, wifi, access_point
 
 def getWifi(interface:str)->wifi.Wifi:
         if sys.platform == "linux":
             return linux._WifiLinux(interface)
         return win32._WifiWin(interface)
+
+def getAccessPoint(interface:str)->access_point.AccessPoint:
+    if sys.pathform == "linux":
+        return access_point.AccessPoint(interface)
+    return None
